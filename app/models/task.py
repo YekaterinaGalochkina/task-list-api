9c8b6f8e71fb1dc4082b0cde0e3a34e47e51bd67
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-05-08 15:05:31 -0700

    Added one-to-many relationship between Goal and Task

--- app/models/task.py
+++ app/models/task.py
@@ -1,13 +1,18 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
 from ..db import db
 from datetime import datetime
 from typing import Optional
+from typing import TYPE_CHECKING
+if TYPE_CHECKING: from .goal import Goal
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=False)
     completed_at: Mapped[Optional[datetime]] = mapped_column(nullable=True)
+    goal_id: Mapped[Optional[int]] = mapped_column(ForeignKey("goal.id"))
+    goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
 
     def to_dict(self):
         return {
